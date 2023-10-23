454a61a3a430358408f950eafa97acd885dba30d
widad <wfarhat170602@gmail.com>
2023-10-23 20:02:04 +0200

    confirmDeliveryAddressPage , currentTourPage, AddDeliveryAddressPage

--- src/views/main_page/add_delivery_address_page.py
+++ src/views/main_page/add_delivery_address_page.py
@@ -1,14 +1,53 @@
-from PyQt6.QtWidgets import QHBoxLayout, QLabel
-
+from PyQt6.QtWidgets import QHBoxLayout, QLabel, QLineEdit, QPushButton, QVBoxLayout
 from src.controllers.navigator.page import Page
-
 
 class AddDeliveryAddressPage(Page):
     def __init__(self):
         super().__init__()
 
-        layout = QHBoxLayout()
+        self.address_label = QLabel("Delivery Address:")
+        self.address_input = QLineEdit()
+        self.add_address_button = QPushButton("Add Address")
+        self.add_address_button.clicked.connect(self.add_address)
+        self.add_address_button.setStyleSheet(
+            "QPushButton {"
+                "background-color: #FFD1DC;"  
+                "border: none;"
+                "color: white;"
+                "padding: 10px 20px;"
+                "text-align: center;"
+                "text-decoration: none;"
+                "display: inline-block;"
+                "font-size: 16px;"
+                "margin: 4px 2px;"
+                "cursor: pointer;"
+                "border-radius: 8px;"
+            "}"
+            "QPushButton:hover {"
+                "background-color: #FFB6C1;"  
+            "}"
+        )
 
-        layout.addWidget(QLabel("AddDeliveryAddressPage works!"))
+        layout = QVBoxLayout()
+
+        layout.addWidget(self.address_label)
+        layout.addWidget(self.address_input)
+        layout.addWidget(self.add_address_button)
 
         self.setLayout(layout)
+
+        self.address_list = []
+
+    def add_address(self):
+        address = self.address_input.text()
+
+        if address:
+            self.address_list.append(address)
+            print(f"Added address: {address}")
+        else:
+            print("Invalid address provided!")
+
+        self.address_input.clear()
+
+    def get_address_list(self):
+        return self.address_list
--- src/views/main_page/confirm_delivery_address_page.py
+++ src/views/main_page/confirm_delivery_address_page.py
@@ -1,7 +1,5 @@
-from PyQt6.QtWidgets import QHBoxLayout, QLabel
-
+from PyQt6.QtWidgets import QHBoxLayout, QPushButton
 from src.controllers.navigator.page import Page
-
 
 class ConfirmDeliveryAddressPage(Page):
     def __init__(self):
@@ -9,6 +7,30 @@ class ConfirmDeliveryAddressPage(Page):
 
         layout = QHBoxLayout()
 
-        layout.addWidget(QLabel("ConfirmDeliveryAddressPage works!"))
+        self.confirm_button = QPushButton("Confirm Address")
+        self.confirm_button.clicked.connect(self.confirm_address)
+        self.confirm_button.setStyleSheet(
+            "QPushButton {"
+                "background-color: #FFD1DC;"  
+                "border: none;"
+                "color: white;"
+                "padding: 10px 20px;"
+                "text-align: center;"
+                "text-decoration: none;"
+                "display: inline-block;"
+                "font-size: 16px;"
+                "margin: 4px 2px;"
+                "cursor: pointer;"
+                "border-radius: 8px;"
+            "}"
+            "QPushButton:hover {"
+                "background-color: #FFB6C1;"  
+            "}"
+        )
+
+        layout.addWidget(self.confirm_button)
 
         self.setLayout(layout)
+
+    def confirm_address(self):
+        print("Address confirmed!")
