b2d25a887ce1350424b13645837f0156e8a72671
Charles-François St-Cyr <charlesf2001@gmail.com>
2023-10-23 12:26:22 +0200

    can add point on map

--- src/models/temporary_map_loader.py
+++ src/models/temporary_map_loader.py
@@ -1,9 +1,9 @@
 """THIS IS ONLY FOR DEVELOPMENT, A BETTER MAP LOADER NEEDS TO BE IMPLEMENTED LATER
 """
+import sys
+import xml.etree.ElementTree as ET
 from dataclasses import dataclass
 from typing import Dict, List
-import xml.etree.ElementTree as ET
-import sys
 
 
 @dataclass
@@ -34,6 +34,12 @@ class Map:
     max_longitude: float
     min_longitude: float
 
+    def get_size(self) -> float:
+        return (self.max_latitude - self.min_latitude) * (
+            self.max_longitude - self.min_longitude
+        )
+
+
 class TemporaryMapLoader:
     def load_map(self) -> Map:
         intersections = {}
@@ -42,17 +48,17 @@ class TemporaryMapLoader:
         min_latitude: float = sys.maxsize
         max_longitude: float = sys.maxsize * -1
         min_longitude: float = sys.maxsize
-        
-        tree = ET.parse('src/assets/largeMap.xml')
-        
+
+        tree = ET.parse("src/assets/largeMap.xml")
+
         for el in tree.getroot():
-            if el.tag == 'intersection':
+            if el.tag == "intersection":
                 intersection = Intersection(
-                    id=int(el.attrib['id']),
-                    latitude=float(el.attrib['latitude']),
-                    longitude=float(el.attrib['longitude'])
+                    id=int(el.attrib["id"]),
+                    latitude=float(el.attrib["latitude"]),
+                    longitude=float(el.attrib["longitude"]),
                 )
-                
+
                 if intersection.latitude > max_latitude:
                     max_latitude = intersection.latitude
                 if intersection.latitude < min_latitude:
@@ -61,19 +67,27 @@ class TemporaryMapLoader:
                     max_longitude = intersection.longitude
                 if intersection.longitude < min_longitude:
                     min_longitude = intersection.longitude
-                    
+
                 intersections[intersection.id] = intersection
-            elif el.tag == 'segment':
+            elif el.tag == "segment":
                 segment = Segment(
-                    name=el.attrib['name'],
-                    origin=intersections[int(el.attrib['origin'])],
-                    destination=intersections[int(el.attrib['destination'])],
-                    length=float(el.attrib['length'])
+                    name=el.attrib["name"],
+                    origin=intersections[int(el.attrib["origin"])],
+                    destination=intersections[int(el.attrib["destination"])],
+                    length=float(el.attrib["length"]),
                 )
                 segments.append(segment)
-                
-        return Map(intersections, segments, max_latitude, min_latitude, max_longitude, min_longitude)
-        
+
+        return Map(
+            intersections,
+            segments,
+            max_latitude,
+            min_latitude,
+            max_longitude,
+            min_longitude,
+        )
+
+
 if __name__ == "__main__":
     loader = TemporaryMapLoader()
     print(loader.load_map())
--- src/views/main_page/map_view.py
+++ src/views/main_page/map_view.py
@@ -1,39 +1,44 @@
-from PyQt6 import QtGui
+from typing import Optional
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QBrush, QMouseEvent, QPen, QWheelEvent
 from PyQt6.QtWidgets import (
-    QWidget,
-    QSizePolicy,
+    QFrame,
     QGraphicsScene,
     QGraphicsView,
-    QGraphicsRectItem,
-    QStackedLayout,
-    QLabel,
-    QFrame
+    QSizePolicy,
 )
-from PyQt6.QtGui import QBrush, QPen, QWheelEvent
-from PyQt6.QtCore import Qt
+from reactivex import Observable, Subject
+
+from src.models.temporary_map_loader import Map, Position, TemporaryMapLoader
 from src.views.utils.theme import Theme
-from typing import Optional
-from src.models.temporary_map_loader import Map, TemporaryMapLoader
 
 
 class MapView(QGraphicsView):
     __scene: Optional[QGraphicsScene] = None
     __map: Optional[Map] = None
     __zoom: int = 0
+    __point_added: Subject[Position] = Subject()
 
     def __init__(self) -> None:
         super().__init__()
 
         self.__set_config()
-        
+
         map_loader = TemporaryMapLoader()
         self.__build_scene(map_loader.load_map())
-        
+
         self.fitMap()
-        
+
+    @property
+    def point_added(self) -> Observable[Position]:
+        return self.__point_added
+
     def fitMap(self):
         if self.__scene:
-            self.fitInView(self.__scene.sceneRect(), Qt.AspectRatioMode.KeepAspectRatioByExpanding)
+            self.fitInView(
+                self.__scene.sceneRect(), Qt.AspectRatioMode.KeepAspectRatioByExpanding
+            )
             self.__zoom = 0
 
     def __build_scene(self, map: Map):
@@ -42,27 +47,27 @@ class MapView(QGraphicsView):
             map.min_longitude,
             map.min_latitude,
             map.max_longitude - map.min_longitude,
-            map.max_latitude - map.min_latitude
+            map.max_latitude - map.min_latitude,
         )
         self.__scene.setBackgroundBrush(QBrush(Qt.GlobalColor.white))
         self.setScene(self.__scene)
-        
+
         for segment in map.segments:
             self.__scene.addLine(
                 segment.origin.longitude,
                 segment.origin.latitude,
                 segment.destination.longitude,
                 segment.destination.latitude,
-                QPen(QBrush(Qt.GlobalColor.black), 0.00005)
+                QPen(QBrush(Qt.GlobalColor.black), 0.00005),
             )
-            
+
     def wheelEvent(self, event: QWheelEvent) -> None:
         if self.__scene:
             if event.angleDelta().y() > 0:
-                factor = 1.01
+                factor = 1 + (self.__map.get_size() * 5)
                 self.__zoom += 1
             else:
-                factor = 0.99
+                factor = 1 - (self.__map.get_size() * 5)
                 self.__zoom -= 1
 
             if self.__zoom > 0:
@@ -72,10 +77,24 @@ class MapView(QGraphicsView):
             else:
                 self.__zoom = 0
 
-    def __set_config(self):
-        self.setTransformationAnchor(
-            QGraphicsView.ViewportAnchor.AnchorUnderMouse
+    def mouseDoubleClickEvent(self, event: QMouseEvent | None) -> None:
+        position = self.mapToScene(event.pos())
+
+        size = 0.0008
+
+        self.__scene.addEllipse(
+            position.x() - size / 2,
+            position.y() - size / 2,
+            size,
+            size,
+            Qt.GlobalColor.red,
+            Qt.GlobalColor.red,
         )
+
+        self.__point_added.on_next(Position(position.x(), position.y()))
+
+    def __set_config(self):
+        self.setTransformationAnchor(QGraphicsView.ViewportAnchor.AnchorUnderMouse)
         self.setResizeAnchor(QGraphicsView.ViewportAnchor.AnchorUnderMouse)
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
         self.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
@@ -83,4 +102,7 @@ class MapView(QGraphicsView):
             QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
         )
         self.setFrameShape(QFrame.Shape.NoFrame)
+        self.setDragMode(QGraphicsView.DragMode.ScrollHandDrag)
+        self.setCursor(Qt.CursorShape.CrossCursor)
+
         Theme.set_background_color(self, "white")
