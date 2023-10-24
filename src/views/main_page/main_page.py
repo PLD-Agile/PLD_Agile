8073d99579f0f7da4060ee2ddeeddfbbf693b569
Charles-François St-Cyr <charlesf2001@gmail.com>
2023-10-24 16:57:06 +0200

    improve map action buttons

--- src/views/main_page/main_page.py
+++ src/views/main_page/main_page.py
@@ -19,12 +19,10 @@ from src.views.modules.main_page_navigation import (
 from src.views.ui.button import Button
 from src.views.ui.button_group import ButtonGroup
 from src.views.utils.theme import Theme
-from typing import List
+from typing import List, Tuple
 
 
 class MainPage(Page):
-    __map_action_buttons: List[QWidget] = []
-    
     def __init__(self):
         super().__init__()
 
@@ -40,24 +38,28 @@ class MainPage(Page):
         map_view = MapView()
 
         buttons_layout = QHBoxLayout()
+        buttons_layout.setAlignment(Qt.AlignmentFlag.AlignRight)
 
-        reset_map_button = Button("Reset zoom")
-        map_zoom_out_button = Button(icon="minus")
-        map_zoom_in_button = Button(icon="plus")
+        reset_map_button, map_zoom_out_button, map_zoom_in_button = self.__build_map_action_buttons(map_view)
         map_zoom_buttons = ButtonGroup([map_zoom_out_button, map_zoom_in_button])
         
-        self.__map_action_buttons += [reset_map_button, map_zoom_out_button, map_zoom_in_button]
-
         buttons_layout.addWidget(reset_map_button)
         buttons_layout.addWidget(map_zoom_buttons)
 
         map_layout.addWidget(map_view)
         map_layout.addLayout(buttons_layout)
 
+        return map_layout
+
+    def __build_map_action_buttons(self, map_view: MapView) -> Tuple[QWidget]:
+        reset_map_button = Button("Reset zoom")
+        map_zoom_out_button = Button(icon="minus")
+        map_zoom_in_button = Button(icon="plus")
+        
         reset_map_button.clicked.connect(map_view.fit_map)
         map_zoom_out_button.clicked.connect(map_view.zoom_out)
         map_zoom_in_button.clicked.connect(map_view.zoom_in)
         
-        map_view.ready.subscribe(lambda ready: [button.setDisabled(not ready) for button in self.__map_action_buttons])
-
-        return map_layout
+        map_view.ready.subscribe(lambda ready: [button.setDisabled(not ready) for button in [reset_map_button, map_zoom_out_button, map_zoom_in_button]])
+        
+        return reset_map_button, map_zoom_out_button, map_zoom_in_button
