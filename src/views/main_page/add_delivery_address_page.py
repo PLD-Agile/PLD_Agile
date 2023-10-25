27b185d90a29444f8e973d211ed6f16d81fda929
widad <wfarhat170602@gmail.com>
2023-10-25 08:24:51 +0200

    some other pages

--- src/views/main_page/add_delivery_address_page.py
+++ src/views/main_page/add_delivery_address_page.py
@@ -5,38 +5,38 @@ class AddDeliveryAddressPage(Page):
     def __init__(self):
         super().__init__()
 
-        self.address_label = QLabel("Delivery Address:")
+        self.address_label = QLabel("<b>Delivery Address:</b>")
         self.address_input = QLineEdit()
         self.add_address_button = QPushButton("Add Address")
         self.add_address_button.clicked.connect(self.add_address)
-        self.add_address_button.setStyleSheet(
-            "QPushButton {"
-                "background-color: #FFD1DC;"  
-                "border: none;"
-                "color: white;"
-                "padding: 10px 20px;"
-                "text-align: center;"
-                "text-decoration: none;"
-                "display: inline-block;"
-                "font-size: 16px;"
-                "margin: 4px 2px;"
-                "cursor: pointer;"
-                "border-radius: 8px;"
-            "}"
-            "QPushButton:hover {"
-                "background-color: #FFB6C1;"  
-            "}"
-        )
 
         layout = QVBoxLayout()
 
-        layout.addWidget(self.address_label)
-        layout.addWidget(self.address_input)
-        layout.addWidget(self.add_address_button)
+        for widget in [self.address_label, self.address_input, self.add_address_button]:
+            widget.setStyleSheet("color: black;")
+            layout.addWidget(widget)
+
+        style_sheet = """
+            QPushButton {
+                background-color: #FFC0CB;  /* Pink */
+                border: none;
+                color: white;
+                padding: 10px 20px;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+                font-size: 16px;
+                margin: 4px 2px;
+                cursor: pointer;
+                border-radius: 8px;
+            }
+            QPushButton:hover {
+                background-color: #FF69B4;  /* Hot Pink on hover */
+            }
+        """
+        self.setStyleSheet(style_sheet)
 
         self.setLayout(layout)
-
-        self.address_list = []
 
     def add_address(self):
         address = self.address_input.text()
