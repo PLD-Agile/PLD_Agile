e6ebac9a4da5947a7fd9efbbf32f9fd0a6682b09
clemgi0 <clement.giraudon2@gmail.com>
2023-11-06 11:10:03 +0100

    Add delivery men list in manage delivery men

--- src/views/manage_delivery_man_page/read_delivery_man.py
+++ src/views/manage_delivery_man_page/read_delivery_man.py
@@ -13,11 +13,14 @@ from src.views.modules.manage_delivery_man_navigator.routes import (
 from src.views.ui import Button, Callout, Separator, Text, TextSize
 from src.views.ui.button_group import ButtonGroup
 from src.views.ui.nav_button import NavigationButton
+from src.services.delivery_man.delivery_man_service import DeliveryManService
 
 
 class ReadDeliveryMan(Page):
     def __init__(self):
         super().__init__()
+
+        delivery_man_service = DeliveryManService()
 
         # Define components to be used in this screen
         layout = QVBoxLayout()
@@ -26,5 +29,19 @@ class ReadDeliveryMan(Page):
         # Add components in the screen
         layout.setAlignment(Qt.AlignmentFlag.AlignTop)
         layout.addWidget(title_label)
+        layout.addLayout(self.__build_deliverymen_list(delivery_man_service))
 
         self.setLayout(layout)
+
+    def __build_deliverymen_list(self, delivery_man_service: DeliveryManService) -> QVBoxLayout:
+        deliveryMen = delivery_man_service.get_delivery_men()
+
+        layout = QVBoxLayout()
+
+
+
+        for index, deliveryMan in enumerate(deliveryMen.values()):
+            deliveryManWidget = Text(str(index+1) + ".  Name: " + deliveryMan.name, TextSize.label)
+            layout.addWidget(deliveryManWidget)
+
+        return layout
