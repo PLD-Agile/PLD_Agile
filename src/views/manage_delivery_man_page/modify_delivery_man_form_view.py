5deac45f0283d144c5b1db54c3eb0141e7a228cf
Charles-François St-Cyr <charlesf2001@gmail.com>
2023-11-15 09:30:04 +0100

    fix checkbox enabled/disabled

--- src/views/manage_delivery_man_page/modify_delivery_man_form_view.py
+++ src/views/manage_delivery_man_page/modify_delivery_man_form_view.py
@@ -181,10 +181,10 @@ class ModifyDeliveryManFormView(Page):
 
             if 0 <= index < len(self.__availabilities_checkboxes):
                 self.__availabilities_checkboxes[index].setChecked(True)
-                if self.__availability_is_in_use(
+                
+                self.__availabilities_checkboxes[index].setDisabled(self.__availability_is_in_use(
                     availability, delivery_man, computed_tours
-                ):
-                    self.__availabilities_checkboxes[index].setDisabled(True)
+                ))
 
     def __availability_is_in_use(
         self,
